cdef36e0b09b25163bb3a12be20c8571b5de21b7
Snow <31964545+ksnow18@users.noreply.github.com>
2017-09-14 12:08:38 -0400

    Add files via upload
    Lizards don't make sounds

---
+++ Lizard.java
@@ -0,0 +1,32 @@
+
+/**
+ * A lizard is a type of animal
+ *
+ * @author Snow
+ * @version 2017-09-14
+ */
+public class Lizard extends Animal
+{
+    /**
+     * Constructor for objects of class Animal - does nothing
+     */
+    public Lizard()
+    {
+    }
+
+    /**
+     * Prints a message about eating food!
+     */
+    public void eat()
+    {
+        System.out.print ("Crickets taste delicious!    ");
+    }
+    
+    /**
+     * Prints a message about making a sound!
+     */
+    public void speak()
+    {
+        System.out.print ("*silence*    ");
+    }
+}
