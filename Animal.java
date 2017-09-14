b587d9916901a16e932cdcb57c79f9ec53d18719
mbuescher <mbuescher@hb.edu>
2017-09-14 11:15:24 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,34 @@
+
+/**
+ * This is a generic Animal.  You will write some inherited Animals!
+ *
+ * @author Michael Buescher & APCS-2017
+ * @version  Unit 2
+ */
+public class Animal
+{
+
+    /**
+     * Constructor for objects of class Animal - does nothing
+     */
+    public Animal()
+    {
+    }
+
+    /**
+     * Prints a message about eating food!
+     */
+    public void eat()
+    {
+        System.out.print ("Food! Yum.    ");
+    }
+    
+    /**
+     * Prints a message about making a sound!
+     */
+    public void speak()
+    {
+        System.out.print ("[Animal Sound]    ");
+    }
+
+}
