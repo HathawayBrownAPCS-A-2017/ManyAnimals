f6d869a12a0a140f4541a143696ffd204bf6da1f
mbuescher <mbuescher@hb.edu>
2017-09-14 12:05:44 -0400

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,33 @@
+
+/**
+ * A Snake is a kind of Animal
+ *
+ * @author Buescher
+ * @version APCS 2017
+ */
+
+public class Snake extends Animal
+{
+    /**
+     * Constructor for objects of class Snake - does nothing
+     */
+    public Snake()
+    {
+    }
+
+    /**
+     * Prints a message about eating food!
+     */
+    public void eat()
+    {
+        System.out.print ("Fainting Goats are so yummy!    ");
+    }
+    
+    /**
+     * Prints a message about making a sound!
+     */
+    public void speak()
+    {
+        System.out.print ("Hisssssssss.....    ");
+    }
+}
