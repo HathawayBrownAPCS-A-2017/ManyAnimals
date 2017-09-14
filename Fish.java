4ce1914b9063715bcbb4d8f1531fcfe5d0b8ad3f
bbai <31964543+bbai2018@users.noreply.github.com>
2017-09-14 12:08:39 -0400

    Add files via upload

---
+++ Fish.java
@@ -0,0 +1,26 @@
+
+/**
+ * A Fish is a type of Animal
+ *
+ * @author Brice
+ * @version APCS 2017
+ */
+public class Fish extends Animal
+{
+
+    /**
+     * Prints a message about eating food!
+     */
+    public void eat()
+    {
+        System.out.print ("Smaller fish and aquatic insects     ");
+    }
+    
+    /**
+     * Prints a message about making a sound!
+     */
+    public void speak()
+    {
+        System.out.print ("[Whistles n' bubbly noises]     ");
+    }
+}
